30bc7c2c184df124025d177dd29edc5b97ebce25
ethan-heo <ethan.hb94@gmail.com>
2025-06-10 17:08:23 +0900

    refactor: core API 변경에 따라 react API를 수정한다

--- packages/react/src/use-media-query.ts
+++ packages/react/src/use-media-query.ts
@@ -1,88 +1,57 @@
-import MatchMedia, { createMatchMedia, DefaultMediaQuery } from 'mq-core';
-import { useEffect, useState, useMemo } from 'react';
+import matchMediaManager, {
+    ChangeCallback,
+    DefaultMediaQuery,
+    MatchMediaHandler,
+    SubscribeResult,
+} from 'mq-core';
+import { useEffect, useState } from 'react';
 
 type Device = DefaultMediaQuery['device'];
 
 type Options = Partial<Record<Device, any>>;
 
-type MatchMediaMap = Map<Device, MatchMedia>;
-
-const initialState = <T extends Options>({
-    options,
-    matchMediaMap,
-    defaultValue,
-}: {
-    defaultValue?: T[keyof T];
-    options: T;
-    matchMediaMap: MatchMediaMap;
-}) => {
-    if (defaultValue) {
-        return defaultValue;
-    }
-
-    for (const [device, module] of Object.entries(options)) {
-        const matchMedia = matchMediaMap.get(device as Device);
-
-        if (!matchMedia) {
-            continue;
-        }
-
-        if (matchMedia.matches()) {
-            return module;
-        }
-    }
-};
-
-const createMatchMediaMap = (options: Options) => {
-    const matchMediaMap = new Map<Device, MatchMedia>();
-
-    if (typeof window === 'undefined') {
-        return matchMediaMap;
-    }
-
-    Object.keys(options).forEach((device) => {
-        matchMediaMap.set(device as Device, createMatchMedia(device as Device));
-    });
-
-    return matchMediaMap;
+const initialModule = (options: Options, handler: MatchMediaHandler) => {
+    return options[handler.matches()!];
 };
 
 function useMediaQuery<T extends Options>(
     options: T,
     defaultValue?: T[keyof T],
 ) {
-    const matchMediaMap = useMemo(() => createMatchMediaMap(options), []);
-    const [module, setModule] = useState<T[Device]>(
-        initialState({ options, matchMediaMap, defaultValue }),
+    const subscribeResults = new Map<Device, SubscribeResult>();
+    const matchMediaHandler = matchMediaManager.createMatchMediaHandler();
+    const [module, setModule] = useState(
+        defaultValue ?? initialModule(options, matchMediaHandler),
     );
 
     useEffect(() => {
-        Object.entries(options).forEach(([device, module]) => {
-            const matchMedia = matchMediaMap.get(device as Device);
-
-            if (!matchMedia) {
-                return;
-            }
-
-            matchMedia.subscribe((ev) => {
-                const matches = (ev.currentTarget as MediaQueryList)!.matches;
-
-                if (matches) {
+        for (const [device, module] of Object.entries(options)) {
+            const subscribeResult = matchMediaHandler.subscribe(
+                device as Device,
+                () => {
                     setModule(module);
-                }
-            });
+                },
+            );
 
-            matchMedia.run();
-        });
+            subscribeResults.set(device as Device, subscribeResult);
+        }
 
         return () => {
-            matchMediaMap.forEach((matchMedia) => {
-                matchMedia.clear();
-            });
+            matchMediaHandler.clear();
         };
     }, []);
 
-    return module as T[keyof T];
+    return {
+        module: module as T[keyof T],
+        updateModule: (device: Device, callback: ChangeCallback) => {
+            const subscribeResult = subscribeResults.get(device);
+
+            if (subscribeResult) {
+                subscribeResult.update(callback);
+                subscribeResult.run();
+            }
+        },
+    };
 }
 
 export default useMediaQuery;
