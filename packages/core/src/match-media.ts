23ebdcf759230040f607be23cc1f5446818a6aad
ethan-heo <ethan.hb94@gmail.com>
2025-06-15 21:34:28 +0900

    feat: MatchMedia 에서 사용하는 window.matchmedia의 인터페이스를 서버 환경에서 사용할 수 있도록 목업을 구현한다

--- packages/core/src/match-media.ts
+++ packages/core/src/match-media.ts
@@ -1,36 +1,36 @@
 import { MediaQuery } from './types';
+import isBrowser from './utils/is-browser';
 
-export type ChangeCallback = (ev: MediaQueryListEvent) => void;
+export type ChangeCallback = () => void;
 
 export type Unsubscribe = () => void;
 
 class MatchMedia {
     #matchMedia: MediaQueryList;
     #callbacks: ChangeCallback[] = [];
-    #listener: ChangeCallback;
-    #pickedCallbacks: ChangeCallback[] | null = null;
+    #listener: (ev: MediaQueryListEvent) => void;
     #skipMatches: boolean = false;
 
     constructor(mediaQuery: MediaQuery) {
         this.#listener = (ev) => {
             if (!this.#skipMatches && !ev.matches) return;
 
-            if (Array.isArray(this.#pickedCallbacks)) {
-                this.#callbacks.forEach(
-                    (cb) => this.#pickedCallbacks?.includes(cb) && cb(ev),
-                );
-                this.#pickedCallbacks = null;
-            } else {
-                this.#callbacks.forEach((cb) => cb(ev));
-            }
+            this.#callbacks.forEach((cb) => cb());
         };
-        this.#matchMedia = window.matchMedia(mediaQuery);
+        this.#matchMedia = this.getMatchMedia(mediaQuery);
         this.#matchMedia.addEventListener('change', this.#listener);
     }
 
     run(...callbacks: ChangeCallback[]) {
-        this.#pickedCallbacks = callbacks.length === 0 ? null : callbacks;
-        this.#matchMedia.dispatchEvent(new Event('change'));
+        if (!this.#skipMatches && !this.#matchMedia.matches) {
+            return;
+        }
+
+        if (callbacks.length > 0) {
+            this.#callbacks.forEach((cb) => callbacks.includes(cb) && cb());
+        } else {
+            this.#callbacks.forEach((cb) => cb());
+        }
     }
 
     unsubscribe(callback: ChangeCallback) {
@@ -58,6 +58,19 @@ class MatchMedia {
 
     skipMatches() {
         this.#skipMatches = true;
+    }
+
+    private getMatchMedia(mediaQuery: MediaQuery) {
+        if (isBrowser()) {
+            return window.matchMedia(mediaQuery);
+        } else {
+            return {
+                matches: true,
+                addEventListener: (..._: any[]) => _,
+                removeEventListener: (_: any) => _,
+                dispatchEvent: (_: Event): any => _,
+            } as unknown as MediaQueryList;
+        }
     }
 }
 
