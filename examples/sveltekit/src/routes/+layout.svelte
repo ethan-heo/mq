90015ebb97cb55bf9b118706296da80a9dd9216d
ethan-heo <ethan.hb94@gmail.com>
2025-06-28 01:09:12 +0900

    feat: Sveltekit 예제를 추가한다

---
+++ examples/sveltekit/src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import { matchMediaManager } from 'svelte-mq';
+    import '../app.css';
+
+    matchMediaManager.createMatchMedia('mobile', '(max-width: 768px)');
+    matchMediaManager.createMatchMedia(
+        'tablet',
+        '(min-width: 769px) and (max-width: 1024px)',
+    );
+    matchMediaManager.createMatchMedia('desktop', '(min-width: 1025px)');
+
+    let { children } = $props();
+</script>
+
+{@render children()}
