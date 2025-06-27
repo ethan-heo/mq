d413d5a79030d97e6483dede20d0e9b0a45ea0de
ethan-heo <ethan.hb94@gmail.com>
2025-06-28 00:09:48 +0900

    feat: Svelte 컴포넌트를 사용한 반응형 처리 예제를 추가한다

---
+++ examples/svelte-style-component/src/App.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import Mobile from './Mobile.svelte';
+    import Tablet from './Tablet.svelte';
+    import Desktop from './Desktop.svelte';
+    import mq from 'svelte-mq';
+
+    const Style = mq({
+        mobile: Mobile,
+        tablet: Tablet,
+        desktop: Desktop,
+    });
+</script>
+
+<svelte:component this={$Style}>
+    <div class={`item red`}></div>
+    <div class={`item green`}></div>
+    <div class={`item blue`}></div>
+</svelte:component>
---
+++ examples/svelte-style-component/src/Tablet.svelte
@@ -0,0 +1,26 @@
+<div class="container">
+    <slot />
+</div>
+
+<style>
+    .container :global {
+        display: flex;
+        min-height: 100vh;
+
+        .item {
+            flex: 1 1 auto;
+        }
+
+        .red {
+            background-color: red;
+        }
+
+        .green {
+            background-color: green;
+        }
+
+        .blue {
+            background-color: blue;
+        }
+    }
+</style>
